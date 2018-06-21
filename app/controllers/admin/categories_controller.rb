45c418364ab7166b67ef7b2c8ee52f4a48722c40
jenniferhsueh <jenhsueh89@hotmail.com>
2018-06-21 22:02:36 +0000

    add category partial index and new

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,34 +1,30 @@
 class Admin::CategoriesController < ApplicationController
-    def index
-        @categories = Category.order(id: :desc).all
-      end
-    
-      def new
-        @categories = Category.new
-      end
-    
-      def create
-        @categories = Category.new(category_params)
-    
-        if @categories.save
-          redirect_to [:admin, :categories], notice: 'Category created!'
-        else
-          render :new
-        end
-      end
-    
-      private
-    
-      def category_params
-        params.require(:category).permit(
-          :name,
-          :description,
-          :category_id,
-          :quantity,
-          :image,
-          :price
-        )
-      end
-    
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
     end
+  end
+  
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+      :category_id,
+      :quantity,
+    )
+  end  
+end
     
