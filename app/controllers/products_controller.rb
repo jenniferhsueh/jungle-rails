5f6484bc8713652244fe620056ab6ba074475fca
jenniferhsueh <jenhsueh89@hotmail.com>
2018-06-23 21:23:21 +0000

    visitors can see reviews but not review form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,13 @@
 class ProductsController < ApplicationController
 
-  before_filter :authorize
-  
   def index
     @products = Product.all.order(created_at: :desc)
   end
 
   def show
     @product = Product.find params[:id]
-
+    @reviews = @product.reviews.all.order(created_at: :desc)
+    @new_review = Review.new
   end
 
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,20 @@
 class ReviewsController < ApplicationController
-  
+
   before_filter :authorize
 
   def create
+    review = Review.new(review_params)
+    if review.save
+      session[review_id] = review.id
+      redirect_to '/'
+    else
+      redirect_to '/'
+    end
   end
 
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
 end
