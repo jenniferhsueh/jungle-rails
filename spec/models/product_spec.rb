a31e1ea28374d933f799905d3ec2dba9589b99aa
jenniferhsueh <jenhsueh89@hotmail.com>
2018-06-26 20:34:04 +0000

    refactor user_spec and product_spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     before (:each) do
-      @category = Category.create(:name => "Shoes")     
+      @category = Category.create(:name => "Shoes")
+      @product = @category.products.create(name: "Heels", price: 5, quantity: 1)
     end
 
     it "product should save successfully" do
-      @product = @category.products.create(:name => "Heels", :price => 5, :quantity => 1)
       expect(@product).to be_present
     end
     
@@ -17,22 +17,20 @@ RSpec.describe Product, type: :model do
     end
 
     it "insist on a valid price" do
-      @product = @category.products.create(:name => "Heels", :price => nil, :quantity => 1)
-      @product.valid?
+      @product.price_cents = nil
       expect(@product).not_to be_valid
       expect(@product.errors.full_messages).to include("Price can't be blank")
     end
     
     it "insist on a valid quantity" do
-      @product = @category.products.create(:price => 5, :quantity => nil)
-      # puts @product.errors.full_messages[0]
+      @product.quantity = nil
       # puts @product.errors.full_messages[1]
       expect(@product).not_to be_valid
       expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it "insist on a valid name" do
-      @product = @category.products.create(:name => nil, :price => 5, :quantity => 1)
+      @product.name = nil
       expect(@product).not_to be_valid
       expect(@product.errors.full_messages).to include("Name can't be blank")
     end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,77 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   describe 'Validations' do
-    it "should save successfully when all required properties exist" do
+    before (:each) do
       @user = User.create(
         first_name: "jenn", 
         last_name: "hsueh", 
         email: "j@j", 
         password: "jennifer", 
         password_confirmation: "jennifer")
+    end
+
+    it "should save successfully when all required properties exist" do
       expect(@user).to be_present
     end
 
     it "insist on a valid first_name" do
-      @user = User.create(
-        first_name: nil, 
-        last_name: "hsueh", 
-        email: "j@j", 
-        password: "jennifer", 
-        password_confirmation: "jennifer")
+      @user.first_name = nil
       expect(@user).not_to be_valid
     end
 
     it "insist on a valid last_name" do
-      @user = User.create(
-        first_name: "jenn", 
-        last_name: nil, 
-        email: "j@j", 
-        password: "jennifer", 
-        password_confirmation: "jennifer")
+      @user.last_name = nil
         expect(@user).not_to be_valid
     end
 
     it "insist on a valid email" do
-      @user = User.create(
-        first_name: "jenn", 
-        last_name: "hsueh", 
-        email: nil,
-        password: "jennifer", 
-        password_confirmation: "jennifer")
+      @user.email = nil
       expect(@user).not_to be_valid
     end
     
     it "insist on a valid password" do
-      @user = User.create(
-        first_name: "jenn", 
-        last_name: "hsueh", 
-        email: "j@j", 
-        password: nil, 
-        password_confirmation: "jennifer")
-        # puts @user.inspect
+      @user.password = nil 
       expect(@user).not_to be_valid
       expect(@user.password_digest).to be_nil
     end
 
     it "insist on a valid password_confirmation" do
-      @user = User.create(
-        first_name: "jenn", 
-        last_name: "hsueh", 
-        email: "j@j", 
-        password: "jennifer", 
-        password_confirmation: nil)
+      @user.password_confirmation = nil
       expect(@user).not_to be_valid
     end
 
     it "insist on unique email" do
-      @user = User.create(
-        first_name: "jenn", 
-        last_name: "hsueh", 
-        email: "j@j", 
-        password: "jennifer",
-        password_confirmation: "jennifer")
       expect(@user).to be_valid
 
       @user1 = User.create(
@@ -79,49 +49,37 @@ RSpec.describe User, type: :model do
         last_name: "petropoulos",
         email: "J@J",
         password: "dimitrios",
-        password_confirmation: "dimitrios")
+        password_confirmation: "dimitrios"
+        )
       expect(@user1).not_to be_valid
     end
 
     it "insist on password with minimum 3 characters" do
-      @user = User.create(
-        first_name: "jenn", 
-        last_name: "hsueh", 
-        email: "j@j", 
-        password: "je", 
-        password_confirmation: "je")
+      @user.password = "je", 
+      @user.password_confirmation = "je"
       expect(@user).not_to be_valid
     end
   end
 
   describe '.authenticate_with_credentials' do
-    it "should authenticate user if email matches password" do
+    before (:each) do
       @user = User.create(
         first_name: "jenn", 
         last_name: "hsueh", 
         email: "j@j", 
         password: "jennifer", 
         password_confirmation: "jennifer")
+    end
+
+    it "should authenticate user if email matches password" do
       expect(User.authenticate_with_credentials("j@j", @user.password)).to be_truthy
     end
  
     it "should authenticate despite empty spaces in email" do
-      @user = User.create(
-        first_name: "jenn", 
-        last_name: "hsueh", 
-        email: "j@j", 
-        password: "jennifer", 
-        password_confirmation: "jennifer")
       expect(User.authenticate_with_credentials("  j@j  ", @user.password)).to be_truthy
     end
 
     it "should authenticate despite capitalized email" do
-      @user = User.create(
-        first_name: "jenn", 
-        last_name: "hsueh", 
-        email: "j@j", 
-        password: "jennifer", 
-        password_confirmation: "jennifer")
       expect(User.authenticate_with_credentials("J@J", @user.password)).to be_truthy
     end
   end
